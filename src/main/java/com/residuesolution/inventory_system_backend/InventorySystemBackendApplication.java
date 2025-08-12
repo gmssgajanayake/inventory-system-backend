09f43ebd6be51bf60249a898e3aebd61db5a7109
G.M.Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2025-08-12 16:33:57 +0530

    Initial commit

---
+++ src/main/java/com/residuesolution/inventory_system_backend/InventorySystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.residuesolution.inventory_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/residuesolution/inventory_system_backend/InventorySystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.residuesolution.inventory_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
